e031867b6e7053fc6282931d5e933f9d9979c87c
Unknown <60050160@kmitl.ac.th>
2018-11-07 22:54:02 +0700

    SI

---
+++ SwapInteger.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SwapInteger
+{
+     
+     public static void main(String[] args)
+     {  
+         Scanner in = new Scanner(System.in);
+         System.out.println("Enter a = ");
+         int a = in.nextInt();
+         System.out.println("Enter b = ");
+         int b = in.nextInt();
+         System.out.println(a+"  "+b);
+         int tmp = a;
+         a = b;
+         b = tmp;
+         System.out.println(a+"  "+b);
+         
+        }
+    }
